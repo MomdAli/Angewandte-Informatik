e859db8a32c6774287f407730265951479f85945
MomdAli <mohammadali.alsaiaf@gmail.com>
2024-10-28 16:03:02 +0100

    Quartz sync: Oct 28, 2024, 4:03 PM

--- Code/SSS/Versuch1/v1.py
+++ Code/SSS/Versuch1/v1.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+green = '#89de56'
+red = '#e44a83'
+yellow = '#e4c94a'
+blue = '#4a99e4'
 
 # Funktion zum Einlesen einer CSV-Datei und Berechnung des Mittelwerts und der Standardabweichung
-def read_csv_file(filename, skip_header=1000, num_samples=50000):
+def read_csv_file(filename, skip_header=1000, num_samples=1000):
     # Einlesen der Datei
     data = np.genfromtxt(
         filename,
@@ -13,36 +16,74 @@ def read_csv_file(filename, skip_header=1000, num_samples=50000):
                     1: lambda s: float(s.decode().replace(',', '.'))}
     )
 
-    relevant_data = data[:num_samples, 1]
-    mean = np.mean(relevant_data)
-    std_dev = np.std(relevant_data)
-
+    relevant_data = data[:num_samples, 1]   # Auswahl der relevanten Daten
+    mean = np.mean(relevant_data)           # Berechnung des Mittelwerts
+    std_dev = np.std(relevant_data)         # Berechnung der Standardabweichung
     return mean, std_dev
 
-# Funktion zum Einlesen und Plotten von Messungen
-def process_and_plot_measurements(directory, base_filename="Messung", file_extension=".csv",
-                                  num_files=20):
-
-    # Durchlaufen aller Dateien von Messung001 bis Messung020
-    for i in range(1, num_files + 1):
-        filename = f"{directory}/{base_filename}{i:03d}{file_extension}"
-
-        # Prüfen, ob die Datei existiert
-        if not os.path.exists(filename):
-            print(f"Datei {filename} nicht gefunden.")
-            continue
-
-        # Plotten des Mittelwerts und der Standardabweichung
-        mean, std_dev = read_csv_file(filename)
-        print(f"Mean: {mean}, Std. Dev.: {std_dev}")
-        plt.errorbar(i, mean, yerr=std_dev, fmt='o', label=f'Measurement {i}')
+# Funktion zur Darstellung der Messungen
+def plot_measurements(x, y, std_dev, title):
+    plt.errorbar(x, y, yerr=std_dev, fmt='o', linestyle='--', label='Messungung von 1 bis 20', color=green)
 
     # Plot-Einstellungen
-    plt.xlabel('Time (s)')
-    plt.ylabel('Voltage (V)')
-    plt.title('Voltage vs Time for Multiple Measurements')
-    plt.legend(loc='upper right', fontsize='small', ncol=2)
+    plt.xlabel('Spannung (V)')
+    plt.ylabel('Distanz (cm)')
+    plt.title(title)
+    plt.legend(loc='upper right', fontsize='small', fancybox=True)
     plt.grid(True)
     plt.show()
 
-process_and_plot_measurements("SSS/messungen")
+# Funktion zur Darstellung der linearen Regression
+def plot_function_data(x, y, x_data, y_data, title, r,
+                       color=red, color_data=blue, linestyle='-'):
+    plt.plot(x, y, linestyle=linestyle, color=color, label='Mean')
+    plt.scatter(x_data, y_data, color=color_data, label='Datenpunkte')
+    plt.xlabel('Spannung (V)')
+    plt.ylabel('Distanz (cm)')
+    l = plt.legend(loc='upper right', fontsize='small', fancybox=True)
+    l.get_texts()[0].set_text(f'Lineare Regression mit r^2={r:.4f}')
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
+
+# Funktion zur Berechnung der linearen Regression
+def logarithmic_linear_regression(x, y):
+    x_log = np.log(x)                   # Logarithmieren der x-Werte
+    y_log = np.log(y)                   # Logarithmieren der y-Werte
+    a, b = np.polyfit(x_log, y_log, 1)  # Berechnung der linearen Regression
+    r = np.corrcoef(x_log, y_log)[0, 1] # Berechnung des Korrelationskoeffiz
+    return a, b, r
+
+
+# * Main
+# Durchlaufen aller Dateien von Messung001 bis Messung020
+x_data = []                     # Spannungswerte
+y_data = list(range(10, 70, 3)) # Distanzwerte
+std_devs = []
+for i in range(1, 21):
+    filename = f"SSS/messungen/Messung{i:03d}.csv"
+
+    mean, std_dev = read_csv_file(filename)
+    x_data.append(mean)
+    std_devs.append(std_dev)
+
+    print(f"Spannung bei {y_data[i-1]} cm: {mean:.2f} V ± {std_dev:.2f} V")
+
+
+# Darstellung der Messungen mit Fehlerbalken für die Standardabweichung
+# plot_measurements(x_data, y_data, std_devs, "Messungen")
+
+
+# Berechnung der linearen Regression und Darstellung der Ergebnisse
+print(f'x_data: {x_data} \ny_data: {y_data}')
+a, b, r = logarithmic_linear_regression(x_data, y_data)
+print(f"Linear regression: a={a}, b={b}, r={r}")
+
+x = np.linspace(x_data[0], x_data[-1], 1000)
+y = np.exp(b) * x ** a
+# plot_function_data(x, y, x_data, y_data, "Linear regression", r**2)
+
+# ! Blatt Messung
+
+width_mean, width_std_dev = read_csv_file("SSS/messungen/Blatt_B.csv")
+height_mean, height_std_dev = read_csv_file("SSS/messungen/Blatt_L.csv")
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,90 +3,92 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [
-    Component.RecentNotes({
-      showTags: false,
-      limit: 3,
-    }),
-  ],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/MomdAli/Angewandte-Informatik",
-      "Discord Server": "https://discord.gg/nwtQs3UMec",
-    },
-  }),
+	head: Component.Head(),
+	header: [],
+	afterBody: [
+		Component.RecentNotes({
+			showTags: false,
+			limit: 3,
+		}),
+	],
+	footer: Component.Footer({
+		links: {
+			GitHub: "https://github.com/MomdAli/Angewandte-Informatik",
+			"Discord Server": "https://discord.gg/nwtQs3UMec",
+		},
+	}),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({
-      title: "Explore a",
-      folderClickBehavior: "collapse",
-      folderDefaultState: "collapsed",
-      filterFn: (node) => !node.displayName.startsWith("📜") && !(["tags"].includes(node.name)),
-    })),
-  ],
-  right: [
-    Component.Graph({
-      localGraph: {
-        drag: true,       // whether to allow panning the view around
-        zoom: true,       // whether to allow zooming in and out
-        depth: 1,         // how many hops of notes to display
-        scale: 1.1,       // default view scale
-        repelForce: 0.5,  // how much nodes should repel each other
-        centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
-        fontSize: 0.6,    // what size should the node labels be?
-        opacityScale: 1,  // how quickly do we fade out the labels when zooming out?
-        removeTags: [],   // what tags to remove from the graph
-        showTags: true,   // whether to show tags in the graph
-      },
-      globalGraph: {
-        drag: true,
-        zoom: true,
-        depth: -1,
-        scale: 0.9,
-        repelForce: 8.75,
-        centerForce: 0.42,
-        linkDistance: 80,
-        fontSize: 1,
-        opacityScale: 2,
-        removeTags: [], // what tags to remove from the graph
-        showTags: true, // whether to show tags in the graph
-      },
-    }),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+	beforeBody: [
+		Component.Breadcrumbs(),
+		Component.ArticleTitle(),
+		Component.ContentMeta(),
+		Component.TagList(),
+	],
+	left: [
+		Component.PageTitle(),
+		Component.MobileOnly(Component.Spacer()),
+		Component.Search(),
+		Component.Darkmode(),
+		Component.DesktopOnly(Component.Explorer({
+			title: "Explore",
+			folderClickBehavior: "collapse",
+			folderDefaultState: "collapsed",
+			filterFn: (node) => !node.displayName.startsWith("📜") && !(["tags"].includes(node.name))
+				&& !node.file?.frontmatter?.explorer?.valueOf(),
+		})),
+	],
+	right: [
+		Component.Graph({
+			localGraph: {
+				drag: true,       // whether to allow panning the view around
+				zoom: true,       // whether to allow zooming in and out
+				depth: 1,         // how many hops of notes to display
+				scale: 1.1,       // default view scale
+				repelForce: 0.5,  // how much nodes should repel each other
+				centerForce: 0.3, // how much force to use when trying to center the nodes
+				linkDistance: 30, // how long should the links be by default?
+				fontSize: 0.6,    // what size should the node labels be?
+				opacityScale: 1,  // how quickly do we fade out the labels when zooming out?
+				removeTags: [],   // what tags to remove from the graph
+				showTags: true,   // whether to show tags in the graph
+			},
+			globalGraph: {
+				drag: true,
+				zoom: true,
+				depth: -1,
+				scale: 0.9,
+				repelForce: 8.75,
+				centerForce: 0.42,
+				linkDistance: 80,
+				fontSize: 1,
+				opacityScale: 2,
+				removeTags: [], // what tags to remove from the graph
+				showTags: true, // whether to show tags in the graph
+			},
+		}),
+		Component.DesktopOnly(Component.TableOfContents()),
+		Component.Backlinks(),
+	],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({
-      title: "=> Explore",
-      folderClickBehavior: "collapse",
-      folderDefaultState: "collapsed",
-      filterFn: (node) => !node.displayName.startsWith("📜") && !(["tags"].includes(node.name)),
-    })),
-  ],
-  right: [],
+	beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+	left: [
+		Component.PageTitle(),
+		Component.MobileOnly(Component.Spacer()),
+		Component.Search(),
+		Component.Darkmode(),
+		Component.DesktopOnly(Component.Explorer({
+			title: "Explore",
+			folderClickBehavior: "collapse",
+			folderDefaultState: "collapsed",
+			filterFn: (node) => !node.displayName.startsWith("📜") && !(["tags"].includes(node.name))
+				&& !node.file?.frontmatter?.explorer?.valueOf(),
+		})),
+	],
+	right: [],
 }
